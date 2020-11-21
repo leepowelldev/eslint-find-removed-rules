7e8b37da6326b9d18468a091683968b822c97fa2
Lee Powell <powell.lee@gmail.com>
2020-11-21 15:47:37 +0000

    test: fix for jest not finding tests

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,8 @@ module.exports = {
   restoreMocks: true,
   roots: ['<rootDir>/src'],
   testMatch: [
-    'src/**/__tests__/**/*.+(ts|tsx|js)',
-    'src/**/?(*.)+(spec|test).+(ts|tsx|js)',
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)',
   ],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
