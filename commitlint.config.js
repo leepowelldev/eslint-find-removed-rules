dffacbb1f246ec8c6570fc0b299482f17f3ab11b
Lee Powell <powell.lee@gmail.com>
2020-11-25 08:27:54 +0000

    chore: added wip to commit message types

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,10 @@
+const { rules } = require('@commitlint/config-conventional');
+const { 'type-enum': typeEnum } = rules;
+const [level, applicable, value] = typeEnum;
+
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [level, applicable, [...value, 'wip']],
+  },
 };
